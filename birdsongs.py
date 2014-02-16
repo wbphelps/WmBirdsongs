353d9f9f4f7cf0175e8af019adb9166d45486221
William Phelps <wbphelps@gmail.com>
2014-02-16 18:28:45 +0000

    translate piface boolean input values

--- birdsongs.py
+++ birdsongs.py
@@ -20,7 +20,8 @@ pfd = pifacedigitalio.PiFaceDigital()
 
 #print "Ready"
 
-lastinp = 0
+lastinp = 8 # unlikely but safe way to start
+inpd = {0:0, 1:1, 2:2, 4:3, 8:4, 16:5, 32:6, 64:7, 128:8} # to translate pfd input to something sane
 
 pygame.mixer.music.load("quail.mp3")
 pygame.mixer.music.play()
@@ -42,29 +43,31 @@ pygame.mixer.music.stop()
 def play(inp):
 
     if (inp == 1):
-        print "play quail"
+#        print "play quail"
         pygame.mixer.music.load("quail.mp3")
         pygame.mixer.music.play()
 
-    if (inp == 2):
-        print "play hawk"
+    elif (inp == 2):
+#        print "play hawk"
         pygame.mixer.music.load("hawk.mp3")
         pygame.mixer.music.play()
 
-    if (inp == 3 or inp == 4): # 3rd switch is input 4
-        print "play lark"
+    elif (inp == 3):
+#        print "play lark"
         pygame.mixer.music.load("meadowlark.mp3")
         pygame.mixer.music.play()
 
-    if (inp == 8):
-        print "stop"
+    elif (inp == 4):
+#        print "stop"
         pygame.mixer.music.stop()
 
 while 1:
 
-    inp =  pfd.input_port.value
+  try:
+    inp = inpd[pfd.input_port.value]
 
     if (inp != lastinp): # state change
+#        print "inp {}".format(inp)
 
         pfd.leds[lastinp-1].turn_off()
         if inp > 0:  pfd.leds[inp-1].turn_on()
@@ -73,6 +76,8 @@ while 1:
         if (inp>0 and inp<9):
             play(inp)
 
+  except:
+    pass
 
 
     
