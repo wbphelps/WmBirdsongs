17e2c50718a60f0ea74d5d3292a2f07c961cc541
William Phelps <wbphelps@gmail.com>
2014-02-16 18:00:22 +0000

    put songs in correct order, 1=quail, 2=hawk, 3=lark

--- birdsongs.py
+++ birdsongs.py
@@ -7,12 +7,23 @@ import pygame
 from time import sleep
 
 pygame.init()
+import signal
+import sys
+ 
+def signal_term_handler(signal, frame):
+    print 'got SIGTERM'
+    sys.exit(0)
+ 
+signal.signal(signal.SIGTERM, signal_term_handler)
 
 pfd = pifacedigitalio.PiFaceDigital()
 
 #print "Ready"
 
 lastinp = 0
+
+pygame.mixer.music.load("quail.mp3")
+pygame.mixer.music.play()
 
 i = 0
 while i < 8:
@@ -25,25 +36,28 @@ while i < 8:
     sleep(0.1)
     i += 1
 
+pygame.mixer.music.stop()
+
+
 def play(inp):
 
     if (inp == 1):
-#        print "play hawk"
-        pygame.mixer.music.load("hawk.mp3")
-        pygame.mixer.music.play()
-
-    if (inp == 2):
-#        print "play lark"
-        pygame.mixer.music.load("meadowlark.mp3")
-        pygame.mixer.music.play()
-
-    if (inp == 3 or inp == 4): # 3rd switch is input 4
-#        print "play quail"
+        print "play quail"
         pygame.mixer.music.load("quail.mp3")
         pygame.mixer.music.play()
 
+    if (inp == 2):
+        print "play hawk"
+        pygame.mixer.music.load("hawk.mp3")
+        pygame.mixer.music.play()
+
+    if (inp == 3 or inp == 4): # 3rd switch is input 4
+        print "play lark"
+        pygame.mixer.music.load("meadowlark.mp3")
+        pygame.mixer.music.play()
+
     if (inp == 8):
-#        print "stop"
+        print "stop"
         pygame.mixer.music.stop()
 
 while 1:
